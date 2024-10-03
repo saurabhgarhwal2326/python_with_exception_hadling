3e3d7c235ac8a6baa24af5f731d9d81b0033de97
aurabhgarhwal3 <saurabhgarhwal353@gmail.com>
2024-10-03 16:46:53 +0530

    basic python

---
+++ basic.py
@@ -0,0 +1,883 @@
+# # grand father == python 
+
+# # # data type 
+# a = 8                          # int
+# print(type(a))
+# print(a)
+
+# # A =12
+# # print( type(A))
+# # print(A)
+
+
+# a = 3.4                            # float
+# print(type(a))
+# print(a)
+
+
+# # A = 10.23
+# # print(type(A))
+# # print(A)
+
+# a = True                            # bool
+# b = False
+# print (type (a))
+
+# # A = True
+# # print(type(A))
+# # print(A)
+
+
+# # A = "kiran"
+# # print(type(A))
+# # print(A)
+
+
+# a = "saurabh "                          # string 
+# print(type(a))
+# print(a)
+
+
+# # str = " jfsdkfkdfkdf"
+# # bool =true
+# # float = 786.454
+# #  
+# # a = 8
+# # b = 8.7
+# # c  = "hello"
+# # A = "hello"
+# # a = A
+# # print(a)
+# # rule of varable na/me
+
+# # 89759475= jfgksbd
+# # A =775
+# # @ = 
+# # ! 
+# # #
+# # $
+# # %
+# # ()=7
+# # print()
+
+# # print(type(a))
+# # print("hello")
+# # datatype
+
+# # int  papa    1 2 3 4 5 5  
+# # float uncle    1.344
+# # char bhuwa h f 
+# # bool bde papa  0 and 1 true false
+# # str bhai "kaskfjkasfjka"
+# # keyword 
+# # print=444
+# # list =7
+# # type =66
+# # print(list)
+# # print(print)
+# # print("hello hiii")
+# # _helo= 88
+# # __hello = 78
+# # hello =77
+# # Start with a Letter or Underscore: A variable name must begin with a letter (a-z, A-Z) or an underscore (_).
+
+# # Valid: variable, _variable
+# # Invalid: 1variable, -variable
+# # Followed by Letters, Numbers, or Underscores: After the first character, variable names can include letters, numbers (0-9), or underscores.
+
+# # Valid: variable1, var_2, var_3_
+# # Invalid: var-2, var$3, var@4
+# # Case Sensitive: Variable names are case sensitive, meaning variable, Variable, and VARIABLE are considered different variables.
+
+# # Example: variable, Variable, and VARIABLE are three distinct variables.
+# # No Spaces: Variable names cannot contain spaces. Use underscores (_) to separate words if needed.
+
+# # Valid: my_variable, long_variable_name
+# # Invalid: my variable, long variable name
+# # Reserved Words: Variable names cannot be any of Python's reserved keywords (e.g., if, for, while, class, try, etc.). These keywords have specific meanings in Python and cannot be used as variable na
+# # int 
+# # float 5.4343
+# # str  "hello hiii"
+# # bool True False
+# # int a 7988
+
+
+
+# # a = 7.4
+# # print(a)
+# # print(type(a))
+
+# # >>>>>>>>>>>>>>>>>>>>>>>>.str<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+# # name = "hello hi baby" 
+# # # print(type(name))
+# # print(name)
+#                                       # indexing
+# # print(name[0])
+# # print(a)
+# # print(name[9])
+#                                       # slicing
+
+# # print(name[0:5]) #0 starrting point and 5 ending point
+#                                         # len
+# # print(len(name))  
+#                                   #connect two string 
+
+# #name = "Saurabh"
+# # last_name = "Garhwal"
+# # total_name =name +""+ last_name  #"             w"
+# # print(total_name)  # SaurabhGarhwal   >>>>>>Saurabh Garhwal   A- B  2-1
+
+# # sourabh in total_name
+# # print("HG" in name)
+# # print(found)  
+
+
+
+# # S
+# # a
+# # u
+# # r
+# # a
+# # b
+# # h
+# # range(1,10) 1 starting point or 10 ending point 
+# # 1
+# # 2exit
+# # 3
+# # 4
+# # 5
+# # 6
+# # 7
+# # 8
+# # 9
+# # 10
+
+# # for x in range(1,11):
+# #     print(x)
+
+# name ="manish kumar"
+# # print(type(name))
+# # print(name)
+
+#                                        # indexing
+
+# print (name[4])
+
+
+                                             
+#                                              #  sciling
+
+# print(name[0:7])
+
+
+# print(len(name))                              
+
+
+# first_name = "manish"
+# second_name = "garhwal"
+# result = first_name+ " " +second_name
+# print(result)
+
+# print("a" in name)
+# print("man" in name)
+# print("saurabh" in name)
+
+
+
+
+
+
+# name = "Govind kumawat"
+# # print(type(name))
+# # print(name)
+#                                          #indexing
+
+# # print(name[0])
+# # print(name[8])
+
+# # print(name[2])   
+# # print(name[4])                               
+# # 
+#                                             # slicing     
+# # print(name[0:6])
+# # print(name[7:14])
+
+
+# print(name[0:7])
+# print(name[7:15])
+
+#                                               #len
+
+# # print(len(name))
+
+# # print(len(name))
+
+#                                         # connect to string
+
+# # first_name = "Govind"
+# # last_name = "kumawat"
+# # total = first_name+last_name
+# # print(total)
+
+
+# # first_name =" kiran"
+# # last_name="bajiya"
+# # total= first_name+last_name
+# # print(total)
+
+
+# # first_name =" kiran "
+# # last_name = " bajiya "
+# # total = first_name+" "+last_name
+# # print(total)
+
+# SAURABH ="KIRAN"
+# GARHWAL="BAJIYA"
+# SK=SAURABH+ " "+GARHWAL
+# print(SK)
+# for x in SAURABH:
+#   print(x)      
+  
+#   for xx in SAURABH:
+#     print(xx)                                     # add space
+
+# # first_name = "Govind"
+# # last_name = "kumawat"
+# # total = first_name+ " "+last_name
+# # print(total)
+
+# Govind kumawat in total_name
+# print("Go" in name)
+# # print("kuma" in name)                                                        repeat
+# print("saurabh " in name)
+
+# print(found)  
+# a =[8,7,78578,78]
+# print(type(a))
+
+# # name = "Saurabh"
+
+# for x in name:
+#     print(x)
+# for x in name:           
+#     print(x)
+
+# # for xx in name
+# #     print(xx)
+# #     print((name))
+# # for xx in a:
+# #     print(xx)
+# # print(len(a))  
+
+
+
+#                                                   # tuple 
+
+# # touple as a data type 
+# # mutltiple data store 
+# #syntax () of tuple
+
+# # saurabh = (2.3,"saurabh",23,23)
+# # print(saurabh)
+
+# # a = (2,3,4,6,7)
+# # print(type(a))
+#                                           # indexing 
+# a = (2,3,4,6,7,[7,3,4,4])    #eska mere ko list ke ander jo mera  3 hai usko acces krna hai
+# # for x in a:
+# #     print(x)
+
+# print(a[5:8])
+
+# # print(a[7])
+
+
+# inner_list = a[5]
+# value= inner_list[1]
+# print(value)
+
+# # print(a[5])
+
+# # print(a[2])
+
+#                                         # slicing 
+# #print(a[2:4])
+
+# # print(len(a))
+
+# # hetrogeunus  data type int float str bool 
+# # a=(3,2,4,5,5,5,5)
+# b=(3,4,5,5,5,8,5)
+# # print(a*b)
+# # print(b[5])
+# # b=b[2]==5  # check the position of element
+# # print(b)
+
+
+#                                                  tuple
+
+
+# A=(20," saurabh", 35.75, ["30,40,90"]) 
+
+# print(A)
+
+
+# print(A[0])
+# print(A[3])
+
+
+# print(A[0:2])
+# print(A[3:4])
+
+# b = (2,4,4,5,8)
+
+
+# t =A+b 
+# print(t)
+
+
+# t = A*b
+# print(t)
+# A = [33,45,65,86,98,97]
+# print(type(A))
+
+# B = [43,45,76,78,98]
+
+# t= A+B
+
+# print(t)
+
+# t = A*B
+# print(t)
+
+# print(len(A))
+
+
+# print(A[2:5])
+
+# print(A[2])
+
+
+
+#                                           #       dictonary 
+
+# key - value = item
+# A={"A":10,"B":20,"C":40}
+# print(type(A))
+# print(A)
+
+
+# print(A.items())
+# print(A.keys())
+# print(A.values())
+# print(A.popitem())
+# print(A.clear())
+# print(A.copy())
+# print(A.update())
+# print(A.pop())
+
+#                                           # DICT
+
+# saurabh ={'p':34,'q':55,'r':88,'s':99}
+# print(type(saurabh))
+# print(saurabh)
+#                                        # dict ma add nahi hota h 
+# print(len(saurabh))
+# print(saurabh[1]) 
+# print(saurabh[2:4]) 
+#                             #       indexing and slicing dono nahi hota h 
+
+
+
+
+#                                    #       TUPLE
+
+# A=(23,'kiran',23.34,["23,45,67"])
+# L=(56,'fghjk',66.78,['78,9,00'])
+# print(type(A))
+# print(A)
+
+# y = A+L
+# print(y)                   # tuple me add hota h 
+# print(len(A))
+# print(A[3])  
+# print(L[2:4])              # indexing and slicing hota h 
+
+
+
+#                                    #    LIST
+
+# K =[34,56,78,90,77]
+# S = [32,64,587,86,9708]
+# print(type(K))
+# print(K)
+# v= K+S
+# print(v)                          # list me add hota h 
+# print(len(K))
+# print(K[2:4])                      # indexing and slicing dono hota h 
+# print(S[3])
+
+
+#                                           #   total karna 
+# S= 34
+# K= 32
+# total= S+K
+# print(total)
+
+#                                           #      tuple
+
+# # tuple hetroginious hota h  
+# #  un changabale hota h 
+# # duplicate data ko allow krta h 
+
+
+                            
+# #                                        list 
+# # list changable bhi hoti 
+# # list hetroginious hoti h  
+# # duplicate data ko bhi allow karti h 
+
+
+# A= [3,4,7,8,1,2,9]
+# print(A[2])
+# print(A[1:5])
+# print(len(A))
+# A.insert(3,99)     #  insert function used with position 
+# print(A)
+# A.insert(2,"govind")
+# print(A)
+
+# A.append(77)                  # last me value ko add karta h 
+# print(A)
+
+# A.append("govind")
+# A.pop(3)                     # last element ko delete karta h 
+# A.reverse()                   # list ko ulta karna 
+# A.remove(45)
+
+# A.sort()                        # assending order me kar deta h 1>10
+# print(A)
+
+#                                                  #   dict
+
+# D ={"A":10,"B":20,"C":30,"D":40} 
+# print  (type (D))
+# print(D)
+
+# print(len(D))
+
+# print(D.keys())
+# print(D.values())
+# print(D.items())
+
+# D.update({"A":33})
+# print(D)
+
+
+
+# #   how to  update dict   #  
+# #   Add   = jodna 
+# #   remove = hata na 
+# #   modify =  
+#                                                                              # dict me for loop laga na
+
+# a= [23,34,65,87]
+# for y in a:
+#        print(y)
+
+# for x in D.keys():
+#     print(x)
+
+# for x in D.values():
+#     print(x)
+
+# for x in D.items():
+#     print(x)
+
+#                                                     #  SET
+
+# # un ordered hota h 
+# # unchanbale hota h 
+# # hetroginioud hota h 
+# # unique hota h 
+
+# s = {23, 23," saurabh ", 12.45}                    #   duplicate deta ko allow nahi karta h 
+# print(type(s))
+# print(s)
+
+# print(len(s))
+# print(s[2])                                               # indexing nahi kar sakte h 
+# print(s[2:3])                                                #  slicing nahi kar sakte h 
+
+# a = {11,"govind",44.6}                                         # add nahi hota h 
+# total= (s+a)
+# print(total)
+
+
+
+#                                                         #                     operators 
+#                                                         # arthimatic operators
+
+# print(2+5)                # addiction 
+# print(30-21)                #saptraction
+# print(36*4)                #  multiplication 
+# print(45/5)                 # division
+# print(34%5)                  # modulous
+# print(5**9)                   #exponentation 
+# print(67//8)                  #floor division
+
+
+#                                                   # compression operators
+
+
+# print(34==34)                # equal
+# print(43!=5)                  # not equal
+# print(78>56)                  # grater than
+# print(78<56)                   # less than
+# print(46>=99)                  #grater than equal to 
+# print(46<=99)                   # less than equal  to 
+
+
+#                                                        # assignmenat operators
+
+# x = 34
+# print(x)
+
+# x =45
+# y = 77
+# t = x+y
+# print(t)
+
+# x = 67
+# y =56
+# t= x-y
+# print(t)
+
+# x= 67
+# y=34
+# t =x*y
+# print(t)
+
+# x= 56
+# y= 45
+# t = x/y
+# print(t)
+
+# x= 33
+# y = 12
+# t= x%y
+# print(t)
+
+# x= 89
+# y =56
+# t = x//y
+# print(t)
+
+# x=76
+# y=2
+# t = x**y
+# print(t)
+
+
+#        #   2  write a program to read your name and print "hello your name " message   
+
+# name = input ("saurabh ")
+# print(name)
+
+#        #   3  write a progarm to read two numbers and print their sum difference product and division
+
+# num1 = 579
+# num2= 22
+
+# result= num1/num2
+# print(result)
+
+#        #   4  write a program to read a string and character count of a given string 
+
+# A = {"a":12,"b":24}
+# print(type(A))
+# print(A)
+
+
+#                                                  #     dict
+
+
+
+
+# dt = {"name":"saurabh ","branch":"cse","roll_no":24}
+
+# print(type(dt))
+# # print(dt)
+
+# print(dt.keys())
+# print(dt.values())
+# print(dt["branch"])
+# print(dt["name"])
+
+
+# dt['name'] = 'raja'
+# print(dt)
+
+
+# dt['branch'] ='electrical'
+# dt['subject']= 'maths'
+# dt["class teacher "]="rajendar ji "
+
+
+# print(dt.keys())
+# print(dt.values())
+
+
+# dt['branch']="computer science"
+# print(dt)
+
+# dt["lacture"]="python "
+# print(dt)
+# print(dt['names'])
+# print(dt.get('names',"not found"))
+
+
+# print(dt.get("Branch","not found"))
+
+# dt1={"A":12,"B":23,"C":45,"D":56}
+# ls=[321,55,65,687,78,98]
+# # print(type(ls))
+# # dt2={"E":78,"F":89,"G":76,"H":99}
+
+
+# # # dt1.update(dt2)
+# # dt1.pop('C')
+# # from functools import fromkeys
+# # ls.pop(1)
+# # dt1.popitem()
+# # ls2 = fromkeys([1,2,3,4,5],"saurabh")
+
+# # print(ls)
+
+# # print(dt1) 
+
+
+# student_data = {'Name':['mohan','rohan','suresh'],
+#                  'marks':[45,23,43],
+#                 'subject':'maths'}
+# student_data['Name'].pop()
+# print(student_data)
+# student_data["marks"].pop()
+# print(student_data)
+
+
+
+# print(type(student_data))
+# print(student_data)
+
+# # student_data["subject"]='physics'
+# # print(student_data)
+# # student_data.pop("subject")
+# # print(student_data)
+# student_data.popitem()
+# print(student_data)
+# student_data['Name'].append('rahul kumar')
+# print(student_data)
+# student_data["marks"].append('88')
+# print(student_data)
+# student_data['college'] ='sobhasaria college '
+# print(student_data)
+
+# ls=[23,66,45,56,78,98,87]
+# print(type(ls))
+# print(ls)
+
+# ls.reverse()
+# print(ls)
+# ls.append(66788)
+# ls.append(999999)
+# print(ls)
+# ls.insert(3,889)
+# ls.insert(5,"saurabh ")
+# print(ls)
+# ls.remove(66)
+# print(ls)
+
+# SK=[234,54,65,57,876,56,867]
+# print(type(SK))
+# print(SK)
+
+# # SK.reverse()
+# # print(SK)
+# SK.append(787878)
+# print(SK)
+# SK.insert(3,"saurabh garhwal")
+# print(SK)
+# SK.remove(234)
+# # print(SK)
+# SK.count(888)
+# print(SK)
+#                                           # assignment operators
+
+# x=5
+# print(x)                   # equal too
+
+# x+=5
+# print(x)                  # + eual to 
+
+# x=-5
+# print(x)
+
+
+
+# x = 4
+# # x=4
+# print(x)
+
+
+# A= 4
+# B=8
+# if A>B:
+#     print("hello ")
+
+# elif B>A:
+#     print("hii hello ")
+
+
+# elif A==B:
+#     print("no hello")
+# else:
+#     print("hiii")
+
+# a=10
+# b=29
+# if a>b: 
+#     print("a b se bada h ")
+# elif b>a:
+#     print("b a se bada h ")
+# elif b==a:
+#     print("b equal to a")
+# elif a==b:
+#     print("a equal to b ")    
+# else:
+#     print("bhai gulat h yea ")    
+
+
+
+
+# a=10
+# b=29
+# if a>b and b>a:                                     #  and     or
+#     print("a b se bada h ")
+# elif b>a:
+#     print("b a se bada h ")
+# elif b==a:
+#     print("b equal to a")
+# elif a==b:
+#     print("a equal to b ")    
+# else:
+#     print("bhai gulat h yea ")    
+
+# z= 88
+# y=99
+# if z>y or y>z:
+#     print("z y se bada h ")
+# elif y>z:
+#      print("y z se bada h ")
+# elif z==y:
+#     print("z equal to y")
+# elif y==z:
+#     print("yequal to z")
+# else:
+#     print("ha yea sahi h ")
+
+
+
+# s=74
+# k=10
+# if s>k:
+#     print("s k se bada h")
+# elif k>s:
+#       print("k s se bada h")
+# elif s==k:
+#     print("s equal h k ke")
+# elif k==s:
+#     print("k equal h s ke" )
+# else:
+#     print("pure hi gulat h ")     
+
+# saurabh = 80
+# kiran =90
+# if saurabh>kiran or kiran>saurabh:
+#     print("saurabh kiran se bada h ")
+# elif kiran>saurabh:
+#     print("kiran saurabh se badi h ")    
+# elif saurabh==kiran:
+#     print("saurabh kiran ke equal h ")
+# elif kiran==saurabh:
+#     print("kiran saurabh ke equal h")
+# else:
+#     print("tum dono hi sahi ho")
+    
+
+
+# a = [2,4,6,6,56,78]
+# for x in a:
+#     print(x)
+
+# d =(13,54,65,76,79,97)
+# for x in a:
+#     print(x)
+# b= " saurabh"
+# for x in b:
+#     print(x)
+
+
+# 1,2,3,4,5,6,7,8,9,10
+
+for x in range(10):
+    print(x)
+
+
+
+# a = [12,324,65,75,87]
+# for x in a:
+#     print(x)
+
+
+                             # while loop 
+# i = 0
+# while i<11:
+#     print(i)                   
+#     i+=2
+
+
+
+# a = 0                                         
+# while a < 20:
+#     a+=1
+#     if a == 10:
+#         continue
+#     print(a)
+
+
+# a =1
+# while a<20:
+#      a+=1
+#      if a ==10:
+#       break
+# print(a)
+
+
+
+# a = 50
+# while a <100:
+#    a+=1
+#    if a == 70:
+#       continue
+#    print(a)
+
+
+
+a=(12,34,45,56,68,89,80,68,57,57,"suarbh garhwal ")
+print(type(a))
+print(a)
+print(a[0])
+print(a[10:22])
+print(len (a))
---
+++ start.py
@@ -0,0 +1,96 @@
+# string 
+# list operation 
+# tuple operation 
+# dict operation 
+# set 
+# condition statement 
+# break 
+# a=continue
+# looping
+# function 
+# oops or concepts 
+
+
+# name="saurabh "
+# last_name="garhwal"
+# print(name[2])
+# print(name[0:5])
+# print(len(name))
+# # print name.count
+# a=name.count('a')
+# print(a)
+# print(name+last_name)
+# print(name+"  "+last_name)
+
+
+# a=[42,45,76,87,89,89,76,54,35,43,56]
+# print(type(a))
+# print(a)
+# print(len(a))
+# print(a[3])
+# print(a[1:8])
+# a.pop()
+# print(a)
+# a.insert(1,888)
+# print(a)
+# a.index(87)
+# print(a)
+# a.reverse()
+# print(a)
+# a.remove(89)
+# print(a)
+# a.count(89)
+
+
+# a=(32,453,67,786,879,8,978,897,
+   # 435)
+# print(type(a))
+# print(a)
+# print(len(a))
+# print(a[2])
+# print(a[2:8])
+# print(a.count(32))
+# for x in a:
+#     if x==978:
+#         break
+#     print(x)
+
+# dt ={"name":"saurabh","branch":"cse","rollno":"234"}
+# print(type(dt))
+# print(dt)
+# print(dt.keys())
+# print(dt.values())
+# print(dt.items())
+
+# for x in dt.keys():
+
+#     print(x)
+
+
+
+
+# for x in dt.items():
+#     print(x)
+
+
+
+# lst 
+# tuple
+# dict
+# set
+# str
+
+a=[321,45,57,789,63,85,789,234]
+for x in a:
+    print(x)
+    if x==85:
+        break
+    print(x)
+    
+
+a=(32,32,35,65,67,98,56,56)
+for x in a:
+    print(x)
+    if x==98:
+        break
+    print(x)
